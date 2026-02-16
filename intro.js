f1beae395291c7e42f5d9720e699e1ff0abbc86c
vicesq17 <A017088492@tec.mx>
2026-02-16 09:14:02 -0600

    JS

--- intro.js
+++ intro.js
@@ -2,3 +2,16 @@ console.log("Hola Mundo")
 console.info("Esto es un mensaje informativo")
 console.warn("Esto es un mensaje de advertencia")
 console.error("Esto es un mensaje de error")
+
+
+//alcance de la variable
+{
+    var Minecraft = "5 estrellas;"
+    let halo = "4 estrellas;"
+}
+
+//la variable minecraft sigue estando afuera del ambito que fue declarada
+console.log(Minecraft);
+
+//Se mabda un error porque la variable halo murio al salir del bloque de codigo donde fue declarada
+console.log(halo);
