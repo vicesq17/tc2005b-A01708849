12fce714356c1e504d5ccbc2482186e232e0a1b5
vicesq17 <A017088492@tec.mx>
2026-02-26 10:33:02 -0600

    lab 12.6

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
