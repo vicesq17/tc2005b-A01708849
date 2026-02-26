58b0f0ea1aeadae242e22e005ee4041d5742591f
vicesq17 <A017088492@tec.mx>
2026-02-26 09:54:49 -0600

    lab 11

--- routes/app.js
+++ routes/app.js
@@ -7,4 +7,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const rutasVideojuegos = require('./routes/videojuegos.routes');
 app.use('/videojuegos', rutasVideojuegos);
 
+app.use((request, response, next) => {
+    response.status(404).send('No hay Videojuegos');
+});
+
 app.listen(3000);
