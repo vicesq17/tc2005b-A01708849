6e5c5e084bfc224df684af219afbede49ff9b1bd
vicesq17 <A017088492@tec.mx>
2026-03-01 18:46:56 -0600

    Lab4: estructura inicial + Ejercicio 1 base

---
+++ lab4-js/lab4.js
@@ -0,0 +1,12 @@
+console.log("JS conectado");
+console.log("Hola Mundo");
+
+
+let numero = prompt("DAME UN NUM");
+numero = Number(numero);
+
+if (isNaN(numero) || numero < 1) {
+    alert("Ingresa un número válido mayor a 0");
+} else {
+
+}
