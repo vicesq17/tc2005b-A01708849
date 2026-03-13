cc075fad4eb7bf21982bd828a352a911c6528bcb
vicesq17 <A017088492@tec.mx>
2026-03-12 21:44:38 -0600

    lab10.2

--- laboratorios 1-11/lab10-rutasyformas/servidor.js
+++ laboratorios 1-11/lab10-rutasyformas/servidor.js
@@ -2,26 +2,68 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((request, response) => {
+    	console.log(`Ruta: ${request.url} | Método: ${request.method}`);
 
-    if (request.url === "/" && request.method === "GET") {
+	// Caso 1: Página principal
+	if (request.url === "/" && request.method === "GET") {
 
-        fs.readFile("index.html", (error, data) => {
-            if (error) {
-                response.writeHead(500, {"Content-Type": "text/plain"});
-                response.write("Error al leer index.html");
-                response.end();
-                return;
-            }
+		fs.readFile("index.html", (error, data) => {
 
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(data);
-            response.end();
-        });
+			if (error) {
+				response.writeHead(500, {"Content-Type": "text/plain"});
+				response.end("Error interno del servidor");
+			}
+			else {
+				response.writeHead(200, {"Content-Type": "text/html"});
+				response.end(data);
+			}
 
-    }
+		});
+	}
+
+	// Caso 2: Ruta adicional
+	else if (request.url === "/info" && request.method === "GET") {
+
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.end("<h1>Ruta adicional del servidor</h1>");
+
+	}
+
+	// Caso 3: Recepción del formulario
+	else if (request.url === "/enviar" && request.method === "POST") {
+
+		let body = '';
+
+		request.on('data', chunk => {
+			body += chunk.toString();
+		});
+
+		request.on('end', () => {
+
+			const nombre = body.split("=")[1];
+
+			fs.appendFile("datos.txt", nombre + "\n", (error) => {
+				if (error) {
+					console.log("Error de gusardado");
+				}
+			});
+
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.end(`<h1>Hola ${nombre}, datos recibidos</h1>`);
+
+		});
+	}
+
+	// Caso 4: Ruta inexistente
+	else {
+
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.end("404 - Ruta no encontrada");
+
+	}
 
 });
 
 server.listen(3000, () => {
-    console.log("Servidor activo en http://localhost:3000");
+	console.log("Servidor activo en http://localhost:3000");
 });
