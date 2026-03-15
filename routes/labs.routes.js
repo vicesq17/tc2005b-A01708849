1d743086b54e8a68f2ab8e80203dc0e4a85c283f
vicesq17 <A017088492@tec.mx>
2026-03-15 17:59:33 -0600

    Guardado de avance intermedio del lab11l

--- routes/labs.routes.js
+++ routes/labs.routes.js
@@ -6,4 +6,6 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'laboratorios 1-11', 'index.html'));
 });
 
+router.get('/about', (req, res) => { res.send('Victor - referencia a profe eduardo por sus labs'); });
+
 module.exports = router;
