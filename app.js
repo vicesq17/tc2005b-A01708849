5a0f5d8aafa85257b2d1d651ebd554257a240fbc
vicesq17 <A017088492@tec.mx>
2026-03-15 19:20:51 -0600

    Lab12 HTML dinamico con EJS, partials y render

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- routes/labs.routes.js
+++ routes/labs.routes.js
@@ -6,6 +6,16 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'laboratorios 1-11', 'index.html'));
 });
 
-router.get('/about', (req, res) => { res.send('Victor - referencia a profe eduardo por sus labs'); });
+router.get('/about', (req, res) => { 
+    res.send('Victor - referencia a profe eduardo por sus labs'); 
+});
+
+router.get('/lab12', (req, res) => {
+    res.render('index', {
+        motor: 'EJS',
+        framework: 'Express',
+        tecnologias: ['Node.js','Express','EJS','Tailwind']
+    });
+});
 
 module.exports = router;
