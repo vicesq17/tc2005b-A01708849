f9c32ea12be2eae02f3f7ac9ffe8d30e7b09984e
vicesq17 <A017088492@tec.mx>
2026-04-19 20:31:49 -0600

    Actualiza conexion de Lab 17 a peleadores_db

--- lab14-sesiones-cookies/util/database.js
+++ lab14-sesiones-cookies/util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'rcortese',
-    password: 'Sebasvic0718'
+    database: 'peleadores_db',
+    password: ''
 });
 
 module.exports = pool.promise();
